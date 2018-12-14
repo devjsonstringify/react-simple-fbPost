f5dac033633f75c027548050d7d6243cf9228952
Glen Mark Cabiladas <gcabiladas@gmail.com>
2018-12-14 15:28:47 +0800

    Modify user name transper from p tag to anchor with href

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const Info = ({ user }) => {
+	const style = {
+		color: '#365899',
+		fontWeight: '600'
+	}
 	return (
 		<React.Fragment>
-			<p className="h6">{user.name}</p>
+			<p className="h6">
+				<a style={style} href={user.userfbURL}>
+					{user.name}
+				</a>
+			</p>
 		</React.Fragment>
 	)
 }

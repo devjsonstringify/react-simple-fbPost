00a2f0220fff74e44a337af4c52d8e70d1189003
Glen Mark Cabiladas <gcabiladas@gmail.com>
2018-12-19 21:56:07 +0800

    Fixed empty id on each row when not define

--- src/components/layout/row.jsx
+++ src/components/layout/row.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Row = ({ children, id, classes }) => {
-	return <div className={classes ? classes : "row"} id={id? id : ""}>{children}</div>
-}
+  return (
+    <div className={classes ? classes : "row"} id={id ? id : null}>
+      {children}
+    </div>
+  );
+};
 
-export default Row
+export default Row;

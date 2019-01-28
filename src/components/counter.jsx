5611196714a1eefffc8fc2f558d11923e46ff14e
Glen Mark Cabiladas <gcabiladas@gmail.com>
2019-01-28 21:21:30 +0800

    Add functionality on like button, this will compute the total liked and custom message

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Counter = ({ counter, text }) => {
   const style = { color: "#606770", fontSize: "14px" };
